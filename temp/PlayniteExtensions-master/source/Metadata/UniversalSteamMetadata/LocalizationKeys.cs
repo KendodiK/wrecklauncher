161de5fa74c956efca05d35f4dbb4c5947de886e
Milan-aka-Suguru <122789724+Milan-aka-Suguru@users.noreply.github.com>
2025-09-16 14:22:52 +0200

    a

---
+++ temp/PlayniteExtensions-master/source/Metadata/UniversalSteamMetadata/LocalizationKeys.cs
@@ -0,0 +1,337 @@
+///
+/// DO NOT MODIFY! Automatically generated via UpdateLocExtFiles.ps1 script.
+/// 
+namespace System
+{
+    public static class LOC
+    {
+        /// <summary>
+        /// Import installed games
+        /// </summary>
+        public const string UniversalSteamMetadataSettingsImportInstalledLabel = "LOCUniversalSteamMetadataSettingsImportInstalledLabel";
+        /// <summary>
+        /// Import not installed games
+        /// </summary>
+        public const string UniversalSteamMetadataSettingsImportUninstalledLabel = "LOCUniversalSteamMetadataSettingsImportUninstalledLabel";
+        /// <summary>
+        /// Connect account
+        /// </summary>
+        public const string UniversalSteamMetadataSettingsConnectAccount = "LOCUniversalSteamMetadataSettingsConnectAccount";
+        /// <summary>
+        /// Checking authentication status…
+        /// </summary>
+        public const string UniversalSteamMetadataLoginChecking = "LOCUniversalSteamMetadataLoginChecking";
+        /// <summary>
+        /// User is authenticated
+        /// </summary>
+        public const string UniversalSteamMetadataLoggedIn = "LOCUniversalSteamMetadataLoggedIn";
+        /// <summary>
+        /// Requires authentication
+        /// </summary>
+        public const string UniversalSteamMetadataNotLoggedIn = "LOCUniversalSteamMetadataNotLoggedIn";
+        /// <summary>
+        /// Failed to authenticate user.
+        /// </summary>
+        public const string UniversalSteamMetadataNotLoggedInError = "LOCUniversalSteamMetadataNotLoggedInError";
+        /// <summary>
+        /// Authenticate
+        /// </summary>
+        public const string UniversalSteamMetadataAuthenticateLabel = "LOCUniversalSteamMetadataAuthenticateLabel";
+        /// <summary>
+        /// Alternative authentication
+        /// </summary>
+        public const string UniversalSteamMetadataAuthenticateAlternativeLabel = "LOCUniversalSteamMetadataAuthenticateAlternativeLabel";
+        /// <summary>
+        /// Start games directly without running official client
+        /// </summary>
+        public const string UniversalSteamMetadataSettingsStartGameDirectly = "LOCUniversalSteamMetadataSettingsStartGameDirectly";
+        /// <summary>
+        /// Troubleshooting account sync issues
+        /// </summary>
+        public const string UniversalSteamMetadataTroubleShootingAccountLink = "LOCUniversalSteamMetadataTroubleShootingAccountLink";
+        /// <summary>
+        /// Troubleshooting issues
+        /// </summary>
+        public const string UniversalSteamMetadataTroubleShootingIssues = "LOCUniversalSteamMetadataTroubleShootingIssues";
+        /// <summary>
+        /// Import all games
+        /// </summary>
+        public const string UniversalSteamMetadataSettingsImportLibraryLabel = "LOCUniversalSteamMetadataSettingsImportLibraryLabel";
+        /// <summary>
+        /// Private Account
+        /// </summary>
+        public const string UniversalSteamMetadataPrivateAccount = "LOCUniversalSteamMetadataPrivateAccount";
+        /// <summary>
+        /// Public Account
+        /// </summary>
+        public const string UniversalSteamMetadataPublicAccount = "LOCUniversalSteamMetadataPublicAccount";
+        /// <summary>
+        /// API Key
+        /// </summary>
+        public const string UniversalSteamMetadataAPIKey = "LOCUniversalSteamMetadataAPIKey";
+        /// <summary>
+        /// Start using {0} client
+        /// </summary>
+        public const string UniversalSteamMetadataStartUsingClient = "LOCUniversalSteamMetadataStartUsingClient";
+        /// <summary>
+        /// Import play time
+        /// </summary>
+        public const string UniversalSteamMetadataImportPlaytime = "LOCUniversalSteamMetadataImportPlaytime";
+        /// <summary>
+        /// Account ID
+        /// </summary>
+        public const string UniversalSteamMetadataAccountID = "LOCUniversalSteamMetadataAccountID";
+        /// <summary>
+        /// Get account ID
+        /// </summary>
+        public const string UniversalSteamMetadataGetAccountID = "LOCUniversalSteamMetadataGetAccountID";
+        /// <summary>
+        /// Metadata language:
+        /// </summary>
+        public const string UniversalSteamMetadataMetadataLanguageLabel = "LOCUniversalSteamMetadataMetadataLanguageLabel";
+        /// <summary>
+        /// What's my account name?
+        /// </summary>
+        public const string SettingsWhatsSteamName = "LOCSettingsWhatsSteamName";
+        /// <summary>
+        /// Account URL Name
+        /// </summary>
+        public const string SteamCustomUrlName = "LOCSteamCustomUrlName";
+        /// <summary>
+        /// Authenticated User
+        /// </summary>
+        public const string SteamLoggedInUser = "LOCSteamLoggedInUser";
+        /// <summary>
+        /// Account is private, API Key is required.
+        /// </summary>
+        public const string SteamRequiresApiKey = "LOCSteamRequiresApiKey";
+        /// <summary>
+        /// Community Hub
+        /// </summary>
+        public const string SteamLinksCommunityHub = "LOCSteamLinksCommunityHub";
+        /// <summary>
+        /// Discussions
+        /// </summary>
+        public const string SteamLinksDiscussions = "LOCSteamLinksDiscussions";
+        /// <summary>
+        /// Guides
+        /// </summary>
+        public const string SteamLinksGuides = "LOCSteamLinksGuides";
+        /// <summary>
+        /// Workshop
+        /// </summary>
+        public const string SteamLinksWorkshop = "LOCSteamLinksWorkshop";
+        /// <summary>
+        /// Download vertical cover images
+        /// </summary>
+        public const string SteamUseVerticalCovers = "LOCSteamUseVerticalCovers";
+        /// <summary>
+        /// Import generic free items
+        /// </summary>
+        public const string SteamImportFreeSubGames = "LOCSteamImportFreeSubGames";
+        /// <summary>
+        /// This will overwrite current categories as well as hidden and favorite states on all Steam games. Do you want to continue?
+        /// </summary>
+        public const string SettingsSteamCatImportWarn = "LOCSettingsSteamCatImportWarn";
+        /// <summary>
+        /// Import Categories?
+        /// </summary>
+        public const string SettingsSteamCatImportWarnTitle = "LOCSettingsSteamCatImportWarnTitle";
+        /// <summary>
+        /// Cannot import categories, account for import is not selected.
+        /// </summary>
+        public const string SettingsSteamCatImportErrorAccount = "LOCSettingsSteamCatImportErrorAccount";
+        /// <summary>
+        /// Cannot import categories, database is not opened.
+        /// </summary>
+        public const string SettingsSteamCatImportErrorDb = "LOCSettingsSteamCatImportErrorDb";
+        /// <summary>
+        /// Failed to import Steam categories: {0}
+        /// </summary>
+        public const string SettingsSteamCatImportError = "LOCSettingsSteamCatImportError";
+        /// <summary>
+        /// Cannot import last played information, account for import is not selected.
+        /// </summary>
+        public const string SettingsSteamLastActivityImportErrorAccount = "LOCSettingsSteamLastActivityImportErrorAccount";
+        /// <summary>
+        /// Cannot import last played information, database is not opened.
+        /// </summary>
+        public const string SettingsSteamLastActivityImportErrorDb = "LOCSettingsSteamLastActivityImportErrorDb";
+        /// <summary>
+        /// Failed to import Steam last played information: {0}
+        /// </summary>
+        public const string SettingsSteamLastActivityImportError = "LOCSettingsSteamLastActivityImportError";
+        /// <summary>
+        /// API Key is required for Steam private accounts
+        /// </summary>
+        public const string SettingsApiKeyTooltip = "LOCSettingsApiKeyTooltip";
+        /// <summary>
+        /// Get API key
+        /// </summary>
+        public const string SettingsApiKeyUrlText = "LOCSettingsApiKeyUrlText";
+        /// <summary>
+        /// Import metadata from Steam account:
+        /// </summary>
+        public const string SettingsSteamImportMetadata = "LOCSettingsSteamImportMetadata";
+        /// <summary>
+        /// Import Steam Categories
+        /// </summary>
+        public const string ImportSteamCategoriesLabel = "LOCImportSteamCategoriesLabel";
+        /// <summary>
+        /// Import Steam Last Played
+        /// </summary>
+        public const string ImportSteamLastActivityLabel = "LOCImportSteamLastActivityLabel";
+        /// <summary>
+        /// Background image source:
+        /// </summary>
+        public const string SteamBackgroundSource = "LOCSteamBackgroundSource";
+        /// <summary>
+        /// Game Image
+        /// </summary>
+        public const string SteamBackgroundSourceImage = "LOCSteamBackgroundSourceImage";
+        /// <summary>
+        /// Store Screenshot
+        /// </summary>
+        public const string SteamBackgroundSourceScreenshot = "LOCSteamBackgroundSourceScreenshot";
+        /// <summary>
+        /// Store Background
+        /// </summary>
+        public const string SteamBackgroundSourceStore = "LOCSteamBackgroundSourceStore";
+        /// <summary>
+        /// Banner
+        /// </summary>
+        public const string SteamBackgroundSourceBanner = "LOCSteamBackgroundSourceBanner";
+        /// <summary>
+        /// Show friends button on Top panel
+        /// </summary>
+        public const string SteamShowFriendsButton = "LOCSteamShowFriendsButton";
+        /// <summary>
+        /// Steam Friends
+        /// </summary>
+        public const string SteamFriendsTooltip = "LOCSteamFriendsTooltip";
+        /// <summary>
+        /// Ignore installed games from other accounts
+        /// </summary>
+        public const string SteamIgnoreOtherAccountGames = "LOCSteamIgnoreOtherAccountGames";
+        /// <summary>
+        /// Additional Accounts
+        /// </summary>
+        public const string SteamAdditionalAccounts = "LOCSteamAdditionalAccounts";
+        /// <summary>
+        /// Specify Steam account ID and API key pair of other accounts if you want to import games from them. See Steam troubleshooting wiki page for more information.
+        /// </summary>
+        public const string SteamAdditionalAccountsDescription = "LOCSteamAdditionalAccountsDescription";
+        /// <summary>
+        /// Limit the amount of tags imported to the first
+        /// </summary>
+        public const string SteamLimitTagCountLabel = "LOCSteamLimitTagCountLabel";
+        /// <summary>
+        /// Prefix tag names with:
+        /// </summary>
+        public const string SteamTagPrefixLabel = "LOCSteamTagPrefixLabel";
+        /// <summary>
+        /// Tag blacklist
+        /// </summary>
+        public const string SteamTagBlacklistTabLabel = "LOCSteamTagBlacklistTabLabel";
+        /// <summary>
+        /// Excluded tags will not be added to games.
+        /// </summary>
+        public const string SteamTagBlacklistExplanationLabel = "LOCSteamTagBlacklistExplanationLabel";
+        /// <summary>
+        /// Available tags
+        /// </summary>
+        public const string SteamTagWhitelistHeader = "LOCSteamTagWhitelistHeader";
+        /// <summary>
+        /// Excluded tags
+        /// </summary>
+        public const string SteamTagBlacklistHeader = "LOCSteamTagBlacklistHeader";
+        /// <summary>
+        /// Remove from excluded tags
+        /// </summary>
+        public const string SteamTagWhitelistTooltip = "LOCSteamTagWhitelistTooltip";
+        /// <summary>
+        /// Add to excluded tags
+        /// </summary>
+        public const string SteamTagBlacklistTooltip = "LOCSteamTagBlacklistTooltip";
+        /// <summary>
+        /// Set tag category as prefix
+        /// </summary>
+        public const string SteamSetTagCategoryAsPrefixLabel = "LOCSteamSetTagCategoryAsPrefixLabel";
+        /// <summary>
+        /// Sets the category the tag belongs to as a prefix. For example, "Action RPG" is turned into "Genres: Action RPG"
+        /// </summary>
+        public const string SteamSetTagCategoryAsPrefixTooltip = "LOCSteamSetTagCategoryAsPrefixTooltip";
+        /// <summary>
+        /// Assessments
+        /// </summary>
+        public const string SteamTagCategoryAssessments = "LOCSteamTagCategoryAssessments";
+        /// <summary>
+        /// Features
+        /// </summary>
+        public const string SteamTagCategoryFeatures = "LOCSteamTagCategoryFeatures";
+        /// <summary>
+        /// Funding
+        /// </summary>
+        public const string SteamTagCategoryFundingEtc = "LOCSteamTagCategoryFundingEtc";
+        /// <summary>
+        /// Genres
+        /// </summary>
+        public const string SteamTagCategoryGenres = "LOCSteamTagCategoryGenres";
+        /// <summary>
+        /// Hardware/Input
+        /// </summary>
+        public const string SteamTagCategoryHardwareInput = "LOCSteamTagCategoryHardwareInput";
+        /// <summary>
+        /// Other Tags
+        /// </summary>
+        public const string SteamTagCategoryOtherTags = "LOCSteamTagCategoryOtherTags";
+        /// <summary>
+        /// Players
+        /// </summary>
+        public const string SteamTagCategoryPlayers = "LOCSteamTagCategoryPlayers";
+        /// <summary>
+        /// Ratings
+        /// </summary>
+        public const string SteamTagCategoryRatingsEtc = "LOCSteamTagCategoryRatingsEtc";
+        /// <summary>
+        /// Software
+        /// </summary>
+        public const string SteamTagCategorySoftware = "LOCSteamTagCategorySoftware";
+        /// <summary>
+        /// Sub-Genres
+        /// </summary>
+        public const string SteamTagCategorySubGenres = "LOCSteamTagCategorySubGenres";
+        /// <summary>
+        /// Themes &amp; Moods
+        /// </summary>
+        public const string SteamTagCategoryThemesMoods = "LOCSteamTagCategoryThemesMoods";
+        /// <summary>
+        /// Top-Level Genres
+        /// </summary>
+        public const string SteamTagCategoryTopLevelGenres = "LOCSteamTagCategoryTopLevelGenres";
+        /// <summary>
+        /// Visuals &amp; Viewpoint
+        /// </summary>
+        public const string SteamTagCategoryVisualsViewpoint = "LOCSteamTagCategoryVisualsViewpoint";
+        /// <summary>
+        /// Fixed tag count must be a valid number
+        /// </summary>
+        public const string SteamValidationFixedTagCount = "LOCSteamValidationFixedTagCount";
+        /// <summary>
+        /// Show Steam's game startup selection menu in desktop mode
+        /// </summary>
+        public const string SteamShowGameLaunchMenuInDesktopMode = "LOCSteamShowGameLaunchMenuInDesktopMode";
+        /// <summary>
+        /// Show Steam's game startup selection menu in fullscreen mode
+        /// </summary>
+        public const string SteamShowGameLaunchMenuInFullscreenMode = "LOCSteamShowGameLaunchMenuInFullscreenMode";
+        /// <summary>
+        /// Import Steam Deck compatibility as a
+        /// </summary>
+        public const string SteamImportSteamDeckCompatibilityAs = "LOCSteamImportSteamDeckCompatibilityAs";
+        /// <summary>
+        /// Should I use Web or API login?
+        /// </summary>
+        public const string SteamWebVsApiLogin = "LOCSteamWebVsApiLogin";
+    }
+}
