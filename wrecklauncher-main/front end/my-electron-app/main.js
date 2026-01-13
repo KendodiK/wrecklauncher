085c1f98665dcc0551355c92c9d623caa87020f0
zumi9i <103568791+zumi9i@users.noreply.github.com>
2026-01-13 12:38:40 +0100

    Initial commit

---
+++ wrecklauncher-main/front end/my-electron-app/main.js
@@ -0,0 +1,39 @@
+// main.js
+const { app, BrowserWindow, ipcMain, Tray, Menu } = require('electron');
+const path = require('path');
+
+let win, tray;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    frame: false,
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+ipcMain.handle('window:minimize', () => win.minimize());
+ipcMain.handle('window:maximize', () => {
+  if (win.isMaximized()) win.unmaximize();
+  else win.maximize();
+});
+ipcMain.handle('window:close', () => win.close());
+ipcMain.handle('window:to-desktop', () => win.hide());
+
+app.whenReady().then(() => {
+  createWindow();
+
+  tray = new Tray(path.join(__dirname, 'icons', 'app-icon.png'));
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Show App', click: () => win.show() },
+    { label: 'Quit', click: () => app.quit() },
+  ]);
+  tray.setToolTip('My Electron App');
+  tray.setContextMenu(contextMenu);
+});
---
+++ wrecklauncher-main/front end/my-electron-app/preload.js
@@ -0,0 +1,13 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// Expose a small, safe API to the renderer
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Generic invoke for compatibility
+  invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
+
+  // Specific window controls (convenience methods)
+  minimize: () => ipcRenderer.invoke('window:minimize'),
+  maximize: () => ipcRenderer.invoke('window:maximize'),
+  close: () => ipcRenderer.invoke('window:close'),
+  toDesktop: () => ipcRenderer.invoke('window:to-desktop'),
+});
