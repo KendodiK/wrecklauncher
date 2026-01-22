85b781aab2484a2adcfee8d87cd83618f79409de
BaBence04 <122789724+Milan-aka-Suguru@users.noreply.github.com>
2026-01-22 11:16:15 +0100

    reworked tokengen and a lil documentation for the homies (☞ﾟヮﾟ)☞☜(ﾟヮﾟ☜)

--- frontend/my-electron-app/main.js
+++ frontend/my-electron-app/main.js
@@ -41,7 +41,12 @@ app.whenReady().then(() => {
 ipcMain.handle('user:get-token', async (event, username) => {
   return await generateToken(username);
 });
-// Save token
+/**
+ * 
+ * saves token to tokenFile, requires fs library
+ * 
+ * @param {string} token the users userid+"."+token from database
+ */
 async function saveToken(token) {
   try {
     await fs.mkdir(path.dirname(tokenFile), { recursive: true });
@@ -51,8 +56,12 @@ async function saveToken(token) {
     console.error('Failed to save token:', err.message);
   }
 }
-
-// Read token (returns null if file not found)
+/**
+ * 
+ * gets token from local file from tokenFile and returns it, requires fs library
+ * 
+ * @returns token or if not found null
+ */
 async function getToken() {
   try {
     const token = await fs.readFile(tokenFile, 'utf-8');
@@ -63,17 +72,21 @@ async function getToken() {
     return null;
   }
 }
-
-// Generate token if file doesn't exist, or read from file
+/**
+ * 
+ * Generates token only if specified or cannot read it from file
+ * 
+ * @param {string} username native username of the user
+ * @param {boolean} generate whether to generate a new token or not, should only generate a new when the app is building itself
+ * @returns {string} token (userid.token is the structure of the string)
+ */
 async function generateToken(username, generate = false) {
   try {
-    // 1️⃣ Try reading existing token
-    if(!generate){
+    // 1️⃣ Try reading existing token    
     let token = await getToken();
-    if (token) {
+    if (token && !generate) {
       console.log('Using saved token:', token);
-      return token;
-    }
+      return token;    
   }
     // 2️⃣ Token doesn't exist → fetch from server
     const serverurl = 'http://localhost:3000';
