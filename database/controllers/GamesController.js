0b4cfcc0f0082a3732b22683dc871d59bc239f60
LCervus <102804734+KendodiK@users.noreply.github.com>
2026-02-02 09:52:26 +0100

    fields added, store games with all data added

--- database/controllers/GamesController.js
+++ database/controllers/GamesController.js
@@ -1,5 +1,7 @@
 const Controller = require('./Controller');
 const PlatformsController = require('./PlatformsController');
+const GenresController = require('./GenresController');
+const GamesGenresController = require('./GamesGenresController');
 
 class GamesController extends Controller {
     constructor() {
@@ -15,8 +17,8 @@ class GamesController extends Controller {
     }
 
     /**
-     * @param {Array} data - ["app_id" = int, "platform_id" = platforms.id, "name" = string, "banner_img" = string, cost = float]
-     * @returns
+     * @param {Array} data - ["app_id" = int, "platform_id" = platforms.id, "name" = string, "banner_img" = string || null, "description" = string || null, "minimum_requirements" = string || null, "cost" = float]
+     * @returns {Array} - ["message": string, "id": int]
      */
     async create(data) {
         super.create();
@@ -26,11 +28,11 @@ class GamesController extends Controller {
             throw foreignKeyCheck;
         }
 
-        const query = 'INSERT INTO `pirate_sites` (app_id, platform_id, name, banner_img, cost) VALUES (?, ?, ?, ?, ?)';
-        const values = [data.app_id, data.platform_id, data.name, data.banner_img, data.cost];
+        const query = 'INSERT INTO `pirate_sites` (app_id, platform_id, name, banner_img, description, minimum_requirements, cost) VALUES (?, ?, ?, ?, ?, ?, ?)';
+        const values = [data.app_id, data.platform_id, data.name, data.banner_img ?? "", data.description ?? "", data.minimum_requirements ?? "", data.cost ?? 0.0];
         try {
             const [result] = await this.dbConnection.execute(query, values);
-            return { message: `${result.id} Element created in table ${this.tableName}` };
+            return { message: `${result.id} Element created in table ${this.tableName}`, id: result.id };
         } catch (err) {
             console.error(`Error while adding new element to table ${this.tableName}: ${err}`);
             throw err;
@@ -39,8 +41,8 @@ class GamesController extends Controller {
 
     /**
      * @param {int} id
-     * @param {Array} data - ["app_id" = int, "platform_id" = platforms.id, "name" = string, "banner_img" = string, cost = float]
-     * @returns
+     * @param {Array} data - ["app_id" = int, "platform_id" = platforms.id, "name" = string, "banner_img" = string || null, "description" = string || null, "minimum_requirements" = string || null, "cost" = float]
+     * @returns {Array} - ["message": string]
      */
     async update(id, data) {
         super.update();
@@ -52,12 +54,14 @@ class GamesController extends Controller {
 
         var old = await this.show(id);
 
-        const query = 'UPDATE `pirate_sites` SET app_id = ?, platform_id = ?, name = ?, banner_img = ?, cost = ? WHERE id = ?;';
+        const query = 'UPDATE `pirate_sites` SET app_id = ?, platform_id = ?, name = ?, banner_img = ?, description = ?, minimum_requirements = ?, cost = ? WHERE id = ?;';
         const values = [
             data.app_id ?? old.app_id, 
             data.platform_id ?? old.platform_id, 
             data.name ?? old.name, 
-            data.banner_img ?? old.banner_img, 
+            data.banner_img ?? old.banner_img,
+            data.description ?? old.description, 
+            data.minimum_requirements ?? old.minimum_requirements, 
             data.cost ?? old.cost, 
             id ];
         try {
@@ -71,6 +75,46 @@ class GamesController extends Controller {
 
     async delete(id) {
         return super.delete(id);
+    }
+
+    /**
+     * Upload games with all related data.
+     * If "platform_id" is null it needs to get a "platform_name" to create a new platform.
+     * @param {Array} data - [
+     *          "app_id" = int, 
+     *          "platform_id" = platform.id || null, 
+     *          "platform_name" = string || null, 
+     *          "name" = string, 
+     *          "banner_img" = string || null, 
+     *          "description" = string || null, 
+     *          "minimum_requirements" = string || null, 
+     *          "cost" = float ]
+     * @param {Array} genre_ids - [ genre.id, ... ] can be null or could contain genres which already exist.
+     * @param {Array} genre_names - [ genre.name, ... ] names for the genres which do not exist yet.
+     * @returns {Array} - ["message": string, "id": int]
+     */
+    async uploadWithAll(data, genre_ids = null, genre_names = null) {
+        if (data.platform_id == null && data.platform_name != null) {
+            const platformsController = new PlatformsController();
+            const platform = await platformsController.create({ "name": data.platform_name });
+            data.platform_id = platform.id;
+        }
+        if (genre_names != null) {
+            const genresController = new GenresController();
+            created_genre_ids = [];
+            for (const genre_name of data.genre_names) {
+                const genre = await genresController.create({ "genre": genre_name });
+                created_genre_ids.push(genre.id);
+            }
+            genre_ids.push.apply(genre_ids, created_genre_ids);
+        }
+        const game = await this.create(data);
+        data.id = game.id;
+
+        const gamesGenresController = new GamesGenresController();
+        for (const genre_id of data.genre_ids) {
+            await gamesGenresController.create({ "game_id": data.id, "genre_id": genre_id });
+        }
     }
 
     async #checkForeignKeys(data) {
--- database/controllers/NativeUsersController.js
+++ database/controllers/NativeUsersController.js
@@ -20,21 +20,27 @@ class NativeUsersController extends Controller {
     }
 
     /**
-     * 
-     * @param {Array} data - name, user_password, pfp (optional)
-     */
+     * @param {Array} data - [
+     *          "token" = string, 
+     *          "name" = string, 
+     *          "user_password" = string, 
+     *          "email" = string, 
+     *          "bio" = string || null, 
+     *          "pfp" = string || null ]
+     * @returns {Array} - ["message": string, "id": int]
+    */
     async create(data) {
         await super.create();
 
         console.log(data.name);
         console.log(data.user_password);
 
-        const query = 'INSERT INTO native_users (token, name, user_password, pfp) VALUES (?, ?, ?, ?);';
-        const values = [this.#generateToken(data.name), data.name, this.#hashPassword(data.user_password), data.pfp || null];
+        const query = 'INSERT INTO native_users (token, name, user_password, email, bio, pfp) VALUES (?, ?, ?, ?, ?, ?);';
+        const values = [this.#generateToken(data.name), data.name, this.#hashPassword(data.user_password), data.email, data.bio || null, data.pfp || null];
 
         try {
             const [result] = await this.dbConnection.execute(query, values);
-            return { message: `${result.id} Element created in table ${this.tableName}` };
+            return { message: `${result.id} Element created in table ${this.tableName}`, id: result.id };
         } catch (err) {
             console.error(`Error while adding new element to table ${this.tableName}: ${err}`);
             throw err;
@@ -44,7 +50,14 @@ class NativeUsersController extends Controller {
     /**
      * 
      * @param {string} id - uuid of the user
-     * @param {Array} data - token (optional true or false), name (optional), user_password (optional), pfp (optional)
+     * @param {Array} data - [
+     *          "token" = string, 
+     *          "name" = string, 
+     *          "user_password" = string, 
+     *          "email" = string, 
+     *          "bio" = string || null, 
+     *          "pfp" = string || null ]
+     * @returns {Array} - ["message": string]
      */
     async update(id, data) {
         await super.update();
@@ -53,8 +66,15 @@ class NativeUsersController extends Controller {
         let name = data.name ?? old.name;
         let token = data.token ? await this.#generateToken(name) : old.token;
         
-        const query = 'UPDATE native_users SET token = ?, name = ?, user_password = ?, pfp = ? WHERE id = ?;';
-        const values = [token, name, data.user_password ?? old.user_password, data.pfp ?? old.pfp, id];
+        const query = 'UPDATE native_users SET token = ?, name = ?, user_password = ?, email = ?, bio = ?, pfp = ? WHERE id = ?;';
+        const values = [
+            token, 
+            name, 
+            data.user_password ?? old.user_password, 
+            data.email ?? old.email, 
+            data.bio ?? old.bio, 
+            data.pfp ?? old.pfp, 
+            id];
 
         try {
             await this.dbConnection.execute(query, values);
