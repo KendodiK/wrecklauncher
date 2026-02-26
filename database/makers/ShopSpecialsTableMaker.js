85d20761ad08acd29956a71aad090d2706236896
LCervus <102804734+KendodiK@users.noreply.github.com>
2026-02-26 13:52:45 +0100

    shop specials table added

--- database/makers/ShopSpecialsTableMaker.js
+++ database/makers/ShopSpecialsTableMaker.js
@@ -1,6 +1,6 @@
 const DatabaseHandler = require("../DatabaseHandler");
 
-class GamesGenresConnectionTableMaker extends DatabaseHandler {
+class GamesTableMaker extends DatabaseHandler {
     constructor() {
         super();
         this.ready = this.getReady();
@@ -14,30 +14,31 @@ class GamesGenresConnectionTableMaker extends DatabaseHandler {
     async create() {
         await this.ready;
         const query = `
-            CREATE TABLE IF NOT EXISTS games_genres_connections (
+            CREATE TABLE shop_specials(
                 game_id MEDIUMINT UNSIGNED NOT NULL, INDEX(game_id),
-                genre_id INT NOT NULL, INDEX(genre_id)
-            )
+                featured BOOLEAN,
+                coming_soon BOOLEAN,
+                discounted BOOLEAN
+            );
         `;
-
         try {
             await this.dbConnection.execute(query);
-            console.log("'Games-genres connection' table created or already exists.");
+            console.log("'Shop_specials' table created or already exists.");
         } catch (err) {
-            console.error("Error creating games-genres table table:", err);
+            console.error("Error creating shop_specials table:", err);
         }
     }
 
     async delete() {
         await this.ready;
-        const query = `DROP TABLE games_genres_connection_table`
+        const query = `DROP TABLE shop_specials`
         try {
             await this.dbConnection.execute(query);
-            console.log("'games_genres_connection_table' table deleted!");
+            console.log("'shop_specials' table deleted!");
         } catch (err) {
-            console.error("Error while deleting 'games_genres_connection_table' table:", err);
+            console.error("Error while deleting 'shop_specials' table:", err);
         }
     }
 }
 
-module.exports = GamesGenresConnectionTableMaker;
+module.exports = GamesTableMaker;
