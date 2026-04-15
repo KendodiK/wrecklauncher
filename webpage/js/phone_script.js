9567f4a622e0415eec49f170a887a173fc801c57
LCervus <102804734+KendodiK@users.noreply.github.com>
2026-04-15 14:01:12 +0200

    website done, js shiting itself

--- webpage/js/phone_script.js
+++ webpage/js/phone_script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const menuImg = document.getElementById("menu-img");
     const menuCont = document.getElementById("menu-cont");
+    const url = "https://api.anchorlauncher.hu/api";
 
     menuImg.addEventListener("click", () => {
         if (menuCont.style.display === "block") {
@@ -15,4 +16,13 @@ document.addEventListener("DOMContentLoaded", async () => {
             menuCont.style.display = "none";
         }
     });
+
+    await fetch(`${url}/games/gamecount`).then(response => response.json())
+        .then(data => {
+            const gameCountElement = document.getElementById("game-count");
+            gameCountElement.textContent = `${data.count}`;
+        }).catch(error => {
+            console.error("Error fetching game count from API:", error);
+        }
+    );
 });
