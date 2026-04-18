8102ded07be46cca335d1b5f676aa793209bfdbd
Milan-aka-Suguru <122789724+Milan-aka-Suguru@users.noreply.github.com>
2026-04-18 22:37:12 +0200

    asd

--- frontend/my-electron-app/csharp/LocalApi/obj/Debug/net8.0/WreckLauncher.LocalApi.AssemblyInfo.cs
+++ frontend/my-electron-app/csharp/LocalApi/obj/Debug/net8.0/WreckLauncher.LocalApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WreckLauncher.LocalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3ac27e37f0650dbde8813460c0205767384ebc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be12a9874b5b3ab68da9d84c9b17ce52462aa1f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WreckLauncher.LocalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WreckLauncher.LocalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
